2ecd57f7345dbf2cca91a8bd6fe3929c66f59b33
Blondy314 <inbar.rotem@gmail.com>
2022-08-10 13:42:14 +0300

    Set meeting with no reminder

--- OOF Guard/Ribbon1.cs
+++ OOF Guard/Ribbon1.cs
@@ -14,45 +14,51 @@ namespace OOF_Guard
         {
             try
             {
-                Application application = Globals.OOFGuard.Application;
-
-                AppointmentItem app = (AppointmentItem)application.CreateItem(OlItemType.olAppointmentItem);
-
-                var ns = application.GetNamespace("MAPI");
-                var user = ns.CurrentUser;
-                var name = user.Name;
-
-                var account = ns.Accounts.Cast<Account>().FirstOrDefault();
-                var addr = name;
-
-                if (account != null)
-                {
-                    addr = account.SmtpAddress;
-                }
-
-                app.Subject = "OOF";
-
-                if (!string.IsNullOrEmpty(name))
-                {
-                    app.Subject = $"{name.Split(' ')[0]} {app.Subject}";
-                }
-
-                app.AllDayEvent = true;
-                app.Location = "OOF";
-
-                app.Recipients.Add(addr);
-                app.Recipients.ResolveAll();
-
-                app.MeetingStatus = OlMeetingStatus.olMeeting;
-                app.ResponseRequested = false;
-
-                app.BusyStatus = OlBusyStatus.olFree;
-
-                app.Display(true);
+                SetupOOFMeeting();
             }
             catch
             {
             }
+        }
+
+        private static void SetupOOFMeeting()
+        {
+            Application application = Globals.OOFGuard.Application;
+
+            AppointmentItem app = (AppointmentItem)application.CreateItem(OlItemType.olAppointmentItem);
+
+            var ns = application.GetNamespace("MAPI");
+            var user = ns.CurrentUser;
+            var name = user.Name;
+
+            var account = ns.Accounts.Cast<Account>().FirstOrDefault();
+            var addr = name;
+
+            if (account != null)
+            {
+                addr = account.SmtpAddress;
+            }
+
+            app.Subject = "OOF";
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                app.Subject = $"{name.Split(' ')[0]} {app.Subject}";
+            }
+
+            app.AllDayEvent = true;
+            app.Location = "OOF";
+            app.ReminderSet = false;
+
+            app.Recipients.Add(addr);
+            app.Recipients.ResolveAll();
+
+            app.MeetingStatus = OlMeetingStatus.olMeeting;
+            app.ResponseRequested = false;
+
+            app.BusyStatus = OlBusyStatus.olFree;
+
+            app.Display(true);
         }
     }
 }
